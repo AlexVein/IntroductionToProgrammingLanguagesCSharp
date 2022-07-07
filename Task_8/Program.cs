1d5deaa622499602632a878e94b80b2e6ee9f37b
Aleksey <alexblackwolf@yandex.ru>
2022-07-07 13:56:38 +0300

    Task 8 completed

---
+++ Task_8/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая на вход принимает число (N),
+ * а на выъоде показывает все четные числа от 1 до N
+ */
+
+Console.Write("Введите целое положительное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+Console.Write($"Четные числа последовательности от 1 до {N}: ");
+
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count++;
+}
