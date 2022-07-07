9985eb29c9de57034c490b344b2756862a860f57
Aleksey <alexblackwolf@yandex.ru>
2022-07-07 20:26:28 +0300

    Task 13 completed

---
+++ Task_13/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая выводиттретью цифру заданного числа
+ * или сообщает, что третьей цифры нет
+ */
+
+const int DigitPos = 3;
+const int NumsInterval = 1500;
+
+Random rand = new Random();
+int num = rand.Next(NumsInterval);
+
+if (num.ToString().Length < DigitPos)
+{
+    Console.WriteLine($"В числе {num} третьей цифры нет.");
+}
+else
+{
+    Console.WriteLine($"Третьей цифрой в числе {num} является - {FindNum(num, DigitPos)}");
+}
+
+
+char FindNum(int num, int digitPos)
+{
+    return num.ToString()[--digitPos];
+}
