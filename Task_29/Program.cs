bc605a1e764b139569b0a77917a5628f240df5de
Aleksey <alexblackwolf@yandex.ru>
2022-07-08 17:16:55 +0300

    Task 29 completed

---
+++ Task_29/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Напишите программу, которая задает массив из 8 элементов,
+ * заполеннный псевдослучайными числами и выводит их на экран
+ */
+
+const int MaxValue = 10;
+const int CountElem = 8;
+
+int[] arr = GenerateArray(CountElem, MaxValue);
+PrintArray(arr);
+
+
+int[] GenerateArray(int countElem, int maxValue)
+{
+    int[] result = new int[countElem];
+    Random random = new Random();
+
+    for (int i = 0; i < countElem; i++)
+    {
+        result[i] = random.Next(MaxValue);
+    }
+
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int element in arr)
+    {
+        Console.Write($"{element} ");
+    }
+}
