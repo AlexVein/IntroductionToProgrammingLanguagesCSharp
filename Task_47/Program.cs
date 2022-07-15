ec425b0871158cb73a3210043707ee4256a11fbf
Aleksey <alexblackwolf@yandex.ru>
2022-07-15 15:21:33 +0300

    Task 47 completed

---
+++ Task_47/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задайте двумерный массив размером MxN,
+ * заполненный случайными вещественными числами
+ */
+
+const int MaxRows = 10;
+const int MaxColumns = 10;
+Random random = new Random();
+
+var rows = random.Next(MaxRows);
+var columns = random.Next(MaxColumns);
+
+var array = GenerateArray(rows, columns, random);
+
+Console.WriteLine($"Rows = {rows}, Columns = {columns}");
+ShowArray(array);
+
+
+double[,] GenerateArray(int rows, int columns, Random random)
+{
+    var result = new double[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = random.NextDouble() * 100;
+        }
+    }
+
+    return result;
+}
+
+
+void ShowArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]:0.##}\t");
+        }
+        Console.WriteLine();
+    }
+}
